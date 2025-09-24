6a9165c4c22a50e109809fd2c5b908b0570fef77
harshit <harshitsharma18000@gmail.com>
2025-09-24 15:01:16 +0530

    Update project with new code

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,  // <-- listen on all network interfaces
+    host: true,  // listen on all network interfaces
     port: 5173,  // default Vite port
+  },
+  build: {
+    minify: 'esbuild', // faster than terser
+    sourcemap: false,  // optional, disables source maps for faster build
   },
 });
